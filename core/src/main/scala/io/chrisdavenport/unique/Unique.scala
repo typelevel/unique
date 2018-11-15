2496a6ccf68ca7dcc9ed2aef2ce931a0230fca84
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-11-15 14:13:30 -0500

    Add Baseline of Other Unique Instances Support

--- core/src/main/scala/io/chrisdavenport/unique/Unique.scala
+++ core/src/main/scala/io/chrisdavenport/unique/Unique.scala
@@ -3,7 +3,9 @@ package io.chrisdavenport.unique
 import cats.Hash
 import cats.effect.Sync
 
-final class Unique private {}
+final class Unique private extends Serializable {
+  override def toString: String = s"Unique(${hashCode.toHexString})"
+}
 object Unique {
   def newUnique[F[_]: Sync]: F[Unique] = Sync[F].delay(new Unique)
 
