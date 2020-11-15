31b24adca963b4efda8a33569fc9b6d6087285fa
Ross A. Baker <ross@rossabaker.com>
2020-11-15 16:09:14 -0500

    Dotty support

--- core/src/test/scala/io/chrisdavenport/unique/UniqueTests.scala
+++ core/src/test/scala/io/chrisdavenport/unique/UniqueTests.scala
@@ -2,21 +2,19 @@ package io.chrisdavenport.unique
 
 import org.scalacheck._
 import cats.effect.IO
-import org.specs2.mutable.Specification
-import org.typelevel.discipline.specs2.mutable.Discipline
 import cats.kernel.laws.discipline.{EqTests, HashTests}
+import munit.DisciplineSuite
 
-
-class UniqueTests extends Specification with Discipline {
+class UniqueTests extends DisciplineSuite {
 
   implicit def functionArbitrary[B, A: Arbitrary]: Arbitrary[B => A] = Arbitrary{
     for {
       a <- Arbitrary.arbitrary[A]
-    } yield {_: B => a}
+    } yield { (_: B) => a}
   }
 
   implicit val uniqueArb : Arbitrary[Unique] = Arbitrary{
-    Arbitrary.arbitrary[Unit].map(_ => Unique.newUnique[IO].unsafeRunSync)
+    Arbitrary.arbitrary[Unit].map(_ => Unique.newUnique[IO].unsafeRunSync())
   }
 
 
