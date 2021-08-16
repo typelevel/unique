57e9997e880518401f955ba4f7fd72ea24f5c5e1
danicheg <e.danicheg@yandex.ru>
2021-08-16 18:47:05 +0300

    Format project

--- core/src/test/scala/org/typelevel/unique/UniqueTests.scala
+++ core/src/test/scala/org/typelevel/unique/UniqueTests.scala
@@ -28,16 +28,15 @@ import munit.DisciplineSuite
 
 class UniqueTests extends DisciplineSuite {
 
-  implicit def functionArbitrary[B, A: Arbitrary]: Arbitrary[B => A] = Arbitrary{
+  implicit def functionArbitrary[B, A: Arbitrary]: Arbitrary[B => A] = Arbitrary {
     for {
       a <- Arbitrary.arbitrary[A]
-    } yield { (_: B) => a}
+    } yield { (_: B) => a }
   }
 
-  implicit val uniqueArb : Arbitrary[Unique] = Arbitrary{
+  implicit val uniqueArb: Arbitrary[Unique] = Arbitrary {
     Arbitrary.arbitrary[Unit].map(_ => Unique.newUnique[SyncIO].unsafeRunSync())
   }
-
 
   checkAll("Unique", HashTests[Unique].hash)
   checkAll("Unique", EqTests[Unique].eqv)
